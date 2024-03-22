144ac4582e6df7758fb4caf47832da060ed12aee
jaeyoung3806 <jaeyoung3806@naver.com>
2024-03-22 17:41:55 +0900

    feat:비밀번호 변경 추가

--- library/src/main/java/com/study/library/controller/AuthController.java
+++ library/src/main/java/com/study/library/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.study.library.controller;
 
 import com.study.library.aop.annotation.ParamsPrintAspect;
 import com.study.library.aop.annotation.ValidAspect;
+import com.study.library.dto.OAuth2MergeReqDto;
+import com.study.library.dto.OAuth2SignupReqDto;
 import com.study.library.dto.SigninReqDto;
 import com.study.library.dto.SignupReqDto;
 import com.study.library.service.AuthService;
@@ -31,9 +33,23 @@ public class AuthController {
         return ResponseEntity.created(null).body(true);
     }
 
+    @ValidAspect
+    @ParamsPrintAspect
+    @PostMapping("/oauth2/signup")
+    public ResponseEntity<?> oAuth2Signup(@Valid @RequestBody OAuth2SignupReqDto oAuth2SignupReqDto, BindingResult bindingResult) {
+        authService.oAuth2Signup(oAuth2SignupReqDto);
+        return ResponseEntity.created(null).body(true);
+    }
+
     @PostMapping("/signin")
     public ResponseEntity<?> signin(@RequestBody SigninReqDto signinReqDto) {
         return ResponseEntity.ok(authService.signin(signinReqDto));
+    }
+    @PostMapping("/oauth2/merge")
+    public ResponseEntity<?> oAuth2Merge(@RequestBody OAuth2MergeReqDto oAuth2MergeReqDto) {
+        authService.oAuth2Merge(oAuth2MergeReqDto);
+        return ResponseEntity.ok(true);
+
     }
 
 }
--- library/src/main/java/com/study/library/service/AuthService.java
+++ library/src/main/java/com/study/library/service/AuthService.java
@@ -1,7 +1,10 @@
 package com.study.library.service;
 
+import com.study.library.dto.OAuth2MergeReqDto;
+import com.study.library.dto.OAuth2SignupReqDto;
 import com.study.library.dto.SigninReqDto;
 import com.study.library.dto.SignupReqDto;
+import com.study.library.entity.OAuth2;
 import com.study.library.entity.User;
 import com.study.library.exception.SaveException;
 import com.study.library.jwt.JwtProvider;
@@ -35,9 +38,24 @@ public class AuthService {
         User user = signupReqDto.toEntity(passwordEncoder);
 
         successCount += userMapper.saveUser(user);
-        successCount += userMapper.saveRole(user.getUserId());
+        successCount += userMapper.saveRole(user.getUserId(), 1);
 
         if(successCount < 2) {
+            throw new SaveException();
+        }
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void oAuth2Signup(OAuth2SignupReqDto oAuth2SignupReqDto) {
+        int successCount = 0;
+        User user = oAuth2SignupReqDto.toEntity(passwordEncoder);
+
+        successCount += userMapper.saveUser(user);
+        successCount += userMapper.saveRole(user.getUserId(), 1);
+        successCount += userMapper.saveOAuth2(oAuth2SignupReqDto.toOAuth2Entity(user.getUserId()));
+
+
+        if(successCount < 3) {
             throw new SaveException();
         }
     }
@@ -54,4 +72,20 @@ public class AuthService {
         return jwtProvider.generateToken(user);
     }
 
+    public void oAuth2Merge(OAuth2MergeReqDto oAuth2MergeReqDto) {
+        User user = userMapper.findUserByUsername(oAuth2MergeReqDto.getUsername());
+
+        if(user == null) {
+            throw new UsernameNotFoundException("사용자 정보를 확인하세요");
+        }
+        if(!passwordEncoder.matches(oAuth2MergeReqDto.getPassword(), user.getPassword())) {
+            throw new BadCredentialsException("사용자 정보를 확인하세요");
+        }
+        OAuth2 oAuth2 = OAuth2.builder()
+                        .oAuth2Name(oAuth2MergeReqDto.getOauth2Name())
+                        .userId(user.getUserId())
+                        .providerName(oAuth2MergeReqDto.getProviderName())
+                        .build();
+        userMapper.saveOAuth2(oAuth2);
+    }
 }
